3f4c961e31a557e6d10100e7b55ddf7d9be641d7
pid011 <pid011@kakao.com>
2020-09-15 15:23:07 +0900

    Add assignments 3

---
+++ assignments/3/dog_info/main.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Dog
+{
+public:
+    Dog(const char* name, int age, int weight)
+    {
+        size_t len = strlen(name) + 1; // 마지막에 '\0' 포함
+        this->name = new char[len];
+        strcpy_s(this->name, len, name);
+        this->age = new int(age);
+        this->weight = new int(weight);
+    }
+
+    ~Dog()
+    {
+        delete[] name;
+        delete age;
+        delete weight;
+    }
+
+    void show_info() const
+    {
+        cout
+            << name << "\n\t"
+            << "Age: " << *age << "\n\t"
+            << "Weight: " << *weight << endl;
+    }
+
+private:
+    char* name;
+    int* age;
+    int* weight;
+};
+
+int main()
+{
+    Dog* happy = new Dog("해피", 3, 4);
+    Dog* zzong_ee = new Dog("쫑이", 4, 5);
+    Dog* ddol_ee = new Dog("똘이", 2, 7);
+
+    happy->show_info();
+    zzong_ee->show_info();
+    ddol_ee->show_info();
+
+    delete happy;
+    delete zzong_ee;
+    delete ddol_ee;
+}
