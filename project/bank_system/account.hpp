a42652878ec7c13f3c75d9167a75338141c92be1
pid011 <pid011@kakao.com>
2020-10-06 08:42:19 +0900

    Add bank_system project

---
+++ project/bank_system/account.hpp
@@ -0,0 +1,127 @@
+﻿#pragma once
+
+#include <cstdio>
+#include <cstring>
+#include <fstream>
+
+namespace BankSystem {
+    class Account {
+    private:
+        static const char* _file_extension;
+
+        char* _name;
+        char* _file_name;
+        int _account_number = 0;
+        int _money = 0;
+
+    public:
+        Account() {
+            _name = new char[30];
+            _file_name = new char[50];
+        }
+
+        Account(const char* name, int account_number, int money) {
+            _name = new char[30];
+            _file_name = new char[50];
+
+            // 이름 동적할당
+            strcpy_s(_name, 30, name);
+
+            // 저장할 파일의 이름 [계좌번호.dat]
+            sprintf_s(_name, 30, "%d", account_number);
+            strcpy_s(_file_name, 50, _name);
+            strcat_s(_file_name, 50, _file_extension);
+
+            _account_number = account_number;
+            _money = money;
+
+            save();
+        }
+
+        ~Account() {
+            // 동적할당한 메모리를 해제해줘야 하는데 오류 발생함
+            if (_name) {
+                delete[] _name;
+            }
+
+            if (_file_name) {
+                delete[] _file_name;
+            }
+        }
+
+        static bool load_account(Account* load_data, int account_number) {
+            using namespace std;
+
+            Account account;
+
+            char tmp[50];
+            sprintf_s(tmp, 50, "%d", account_number);
+
+            size_t ext_length = strlen(_file_extension);
+            size_t file_name_length = strlen(tmp) + ext_length + 1;
+            char* file_name = new char[file_name_length];
+
+            strcpy_s(file_name, file_name_length, tmp);
+            strcat_s(file_name, file_name_length, _file_extension);
+
+            ifstream fs(file_name, ios::in | ios::binary);
+            if (!fs) {
+                return false;
+            }
+
+            fs.read((char*)load_data, sizeof(Account));
+            fs.close();
+            delete[] file_name;
+
+            return true;
+        }
+
+        static const char* get_file_extension() {
+            return _file_extension;
+        }
+
+        const char* get_name() const {
+            return _name;
+        }
+
+        const int* get_account_number() const {
+            return &_account_number;
+        }
+
+        const int* get_money() const {
+            return &_money;
+        }
+
+        // 현재 인스턴스를 파일에 저장
+        bool save() {
+            using namespace std;
+
+            ofstream fs(_file_name, ios::out | ios::binary);
+            fs.write((char*)this, sizeof(Account));
+            fs.close();
+            return true;
+        }
+
+        // 입금
+        bool deposit(int money) {
+            if (money <= 0) {
+                return false;
+            }
+
+            _money += money;
+            return save();
+        }
+
+        // 출금
+        bool withdraw(int money) {
+            if (_money < money) {
+                return false;
+            }
+
+            _money -= money;
+            return save();
+        }
+    };
+
+    const char* Account::_file_extension = ".dat";
+}
---
+++ project/bank_system/main.cpp
@@ -0,0 +1,137 @@
+﻿#include "account.hpp"
+#include <iostream>
+
+using namespace BankSystem;
+using namespace std;
+
+void menu_create() {
+    cout << "[계좌개설]" << endl;
+
+    char name[50];
+    int account_number;
+    int money;
+
+    cout << "계좌번호 : ";
+    cin >> account_number;
+    cin.ignore();
+    cout << "이름 : ";
+    cin.getline(name, 50);
+    cout << "입금액 : ";
+    cin >> money;
+    cin.ignore();
+
+    Account acnt = Account(name, account_number, money);
+    cout << "계좌개설완료" << endl;
+}
+
+void menu_deposit() {
+    cout << "[입금]" << endl;
+
+    int account_number;
+    int money;
+
+    cout << "계좌번호 : ";
+    cin >> account_number;
+    cin.ignore();
+    cout << "입금액 : ";
+    cin >> money;
+    cin.ignore();
+
+    Account acnt;
+    bool result = Account::load_account(&acnt, account_number);
+
+    if (!result) {
+        cout << "계좌번호가 다릅니다." << endl;
+    }
+
+    bool deposit_result = acnt.deposit(money);
+
+    if (!deposit_result) {
+        cout << "입금실패" << endl;
+    }
+    cout << "입금완료" << endl;
+}
+
+void menu_withdraw() {
+    cout << "[출금]" << endl;
+
+    int account_number;
+    int money;
+
+    cout << "계좌번호 : ";
+    cin >> account_number;
+    cin.ignore();
+    cout << "출금액 : ";
+    cin >> money;
+    cin.ignore();
+
+    Account acnt;
+    bool result = Account::load_account(&acnt, account_number);
+
+    if (!result) {
+        cout << "계좌번호가 다릅니다." << endl;
+    }
+
+    bool withdraw_result = acnt.deposit(money);
+
+    if (!withdraw_result) {
+        cout << "출금실패" << endl;
+    }
+    cout << "출금완료" << endl;
+}
+
+void menu_view() {
+    cout << "[잔액조회]" << endl;
+
+    int account_number;
+
+    cout << "계좌번호 : ";
+    cin >> account_number;
+    cin.ignore();
+
+    Account acnt;
+    bool result = Account::load_account(&acnt, account_number);
+
+    if (!result) {
+        cout << "계좌번호가 다릅니다." << endl;
+    }
+
+    cout << "잔액 : " << acnt.get_money() << endl;
+}
+
+int main() {
+    int input = 0;
+
+    while (input != 5) {
+        cout << "--- Menu ---" << endl;
+        cout << "1. 계좌개설" << endl;
+        cout << "2. 입금" << endl;
+        cout << "3. 출금" << endl;
+        cout << "4. 잔액조회" << endl;
+        cout << "5. 프로그램 종료" << endl;
+
+        cout << "선택: ";
+        cin >> input;
+        cin.ignore();
+
+        switch (input) {
+        case 1:
+            menu_create();
+            break;
+        case 2:
+            menu_deposit();
+            break;
+        case 3:
+            menu_withdraw();
+            break;
+        case 4:
+            menu_view();
+            break;
+        case 5:
+            break;
+        default:
+            cout << "다시 선택해주세요." << endl;
+            break;
+        }
+    }
+}
